9e128c5e0d75eff0d7839cf610e7ee1ed35092ba
Alexis Jacob <ferjacobka@gmail.com>
2026-07-16 21:43:57 -0400

    while mejorado para array

--- src/main/java/BucleFor.java
+++ src/main/java/BucleFor.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class BucleFor {
     static void main() {
         for (int i = 0; i <= 10; i++) {
@@ -8,6 +12,36 @@ public class BucleFor {
 
         for (int i = 0; i < dias.length; i++) {
             System.out.println("indice numero: " + i + " es el dia de la semana: " + dias[i]);
+        }
+
+        // for each
+        for (String name: dias) {
+            System.out.println(name);
+        }
+
+        // para un set
+        HashSet<String> countries = new HashSet<>();
+
+        countries.add("Argentina");
+        countries.add("Chile");
+        countries.add("España");
+        countries.add("Peru");
+
+        for (String pais: countries) {
+            System.out.println(pais);
+        }
+
+        // para un map
+        HashMap<String, String> names = new HashMap<>();
+
+        names.put("Nombre", "Alexis");
+        names.put("Apellido", "Jacob");
+        names.put("Sobrenombre", "Jhon");
+
+        for (Map.Entry<String, String> claves: names.entrySet()) {
+            System.out.println("clave + valor: " + claves);
+            System.out.println("solo claves: " + claves.getKey());
+            System.out.println("solo values: " + claves.getValue());
         }
     } // fin function
 } // fin class
--- src/main/java/BucleWhile.java
+++ src/main/java/BucleWhile.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class BucleWhile {
     static void main() {
         int num = 0;
@@ -5,6 +7,24 @@ public class BucleWhile {
         while (num < 5) {
             System.out.println("Numero: " + num);
             num++;
+        }
+
+        // para un array
+        String[] dias = {"LUN", "MAR", "MIE", "JUE", "VIE", "SAB", "DOM"};
+
+        int i = 0;
+        while (i < dias.length) {
+            System.out.println(dias[i]);
+            i++;
+        }
+
+        i = 0;
+        while (i < dias.length) {
+            if (dias[i].equals("MIE")){
+                i+=2;
+            }
+            System.out.println(dias[i]);
+            i++;
         }
     }
 }
