ebce4dabb6c9b71147de7fb222c3e3ab22937b4c
Alexis Jacob <ferjacobka@gmail.com>
2026-07-16 22:40:59 -0400

    functions

--- src/main/java/FuncionesMetodos.java
+++ src/main/java/FuncionesMetodos.java
@@ -9,10 +9,11 @@ public class FuncionesMetodos {
 
         sendEmailToUser("java@gmail.com");
 
-        sendEmailToUser("java@gmail.com", "javadoc");
-
         var users = new ArrayList<>(Arrays.asList("java@gmail.com", "java1@gmail.com", "java2@gmail.com"));
         sendEmailToUser(users);
+        var state = sendEmailToUserWhitState("javadoc@gmail.com");
+        System.out.println(state);
+        System.out.println(sendEmailToUserWhitState("")); // email vacio
     }
 
     // function sin parametros ni retornos
@@ -20,19 +21,26 @@ public class FuncionesMetodos {
         System.out.println("Se envia email");
     }
 
-    // function con parametros
+    // function con parametros y no retornos
     public static void sendEmailToUser(String email) {
         System.out.println("Se envia email a: " + email);
     }
 
     //sobrecarga es cuando tenemos misma funciones pero con comportamientos diferentes
-    public static void sendEmailToUser(String email, String name) {
-        System.out.println("Se envia email a: " + name + " (" + email + ")");
-    }
-
     public static void sendEmailToUser(ArrayList<String> emails) {
         for (String email: emails) {
             System.out.println("Email enviado a: " + email);
         }
+    }
+
+    // function con retorno
+    public static boolean sendEmailToUserWhitState(String email) {
+        if (email == null || email.isBlank()) {
+            System.out.println("El email es nulo");
+            return false;
+        }
+        System.out.println("Se envia email a : " + email );
+
+        return true;
     }
 }
