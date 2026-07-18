fefde928e3b810cf56bc8e22efcf952862c6ca75
Alexis Jacob <ferjacobka@gmail.com>
2026-07-17 20:18:37 -0400

    modificadores de acceso, set y get (repasando)

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,19 +2,19 @@ public class Person {
 
     // atributos
     // se acceden con Person.
-    private String name;
+    private final String name;
     private int age;
-    final private String dni; // sirve para que no se modifique en otros lados ya que es un dato sensible
+    final private String dni; // sirve para que no se modifique en otros lados, ya que es un dato sensible
 
     // constructores
-    // es un mecanismo de inicializacion
+    // es un mecanismo de inicialización
     public Person(String name, int age, String dni) {
-        this.name = name; // con this. llamamos al age y name de la class person algo asi como el padre
+        this.name = name; // Con this. Llamamos al age y name de la class person algo asi como el padre
         this.setAge(age); // se pone asi porque hay un criterio para la edad
         this.dni = dni;
     }
 
-    // functions metodos
+    // functions métodos
     public void sayHello() {
         System.out.println("hola, soy " + name + " y tengo " + age + " años y mi numero de dni es: " + dni);
     }
@@ -29,7 +29,7 @@ public class Person {
         if (age > 0) {
             this.age = age;
         } else {
-            System.out.println("Edad no valdia");
+            System.out.println("Edad no valida");
         }
     }
 }
