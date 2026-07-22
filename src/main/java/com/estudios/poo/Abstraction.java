87f111f90acc4b9122af8f3c8140d0ecd19fdcbe
Alexis Jacob <ferjacobka@gmail.com>
2026-07-21 21:03:33 -0400

    Chore: abs en estudio

--- src/main/java/com/estudios/poo/Abstraction.java
+++ src/main/java/com/estudios/poo/Abstraction.java
@@ -1,7 +1,34 @@
 package com.estudios.poo;
 
-
 public class Abstraction {
-    
-    
+
+    public static void main(String[] args) {
+        
+        // var animalito = new Animal(); una clase abstracta no puede ser instanciada
+        var doggy = new Dog();
+        doggy.Sleep(); // si es una herencia si se puede instanciar
+        
+        var kitty = new Cat();
+        kitty.Sleep();
+    }
+
+    // clase abstracta
+    public static abstract class Animal { // se ocupa palabra reservada
+
+        public void Sleep() {
+            System.out.println("EL animal duerme");
+        }
+    }
+    public static class Dog extends Animal {
+
+        @Override
+        public void Sleep() {
+            System.out.println("El perro duerme");
+        }
+        
+    }
+    public static class Cat extends Animal {
+        
+    }
+
 }
