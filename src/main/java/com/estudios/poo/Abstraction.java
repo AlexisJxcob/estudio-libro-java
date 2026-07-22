93d58eeb59c6a031419cd3694064287dfdb4b8cb
Alexis Jacob <ferjacobka@gmail.com>
2026-07-21 20:56:31 -0400

    Chore: polimorfismo

---
+++ src/main/java/com/estudios/poo/Abstraction.java
@@ -0,0 +1,7 @@
+package com.estudios.poo;
+
+
+public class Abstraction {
+    
+    
+}
--- src/main/java/com/estudios/poo/Polimorfismo.java
+++ src/main/java/com/estudios/poo/Polimorfismo.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.estudios.poo;
 
-/**
- *
- * @author alexis
- */
 public class Polimorfismo {
-    
+
+    public static void main(String[] args) {
+
+        var animal = new Animal();
+        animal.Sound();
+
+        var dog = new Dog();
+        dog.Sound();
+
+        // polimorfismo por sobrecarga
+        var calculadora = new Calculator();
+        System.out.println(calculadora.sumar(3, 6));
+        System.out.println(calculadora.sumar(3.3, 6.6));
+    }
+
+    // poliformismo permite que distintos objetos respondan de forma diferente
+    public static class Animal {
+
+        public void Sound() {
+            System.out.println("Hace un ruido");
+        }
+    }
+
+    public static class Dog extends Animal {
+
+        // polimorfismo por herencia
+        @Override
+        public void Sound() {
+            System.out.println("El perro hace guau");
+            // - super.Sound();  cambia el funcionamiento del padre
+        }
+
+    }
+
+    // polimorfismo por sobrecarga
+    public static class Calculator {
+
+        public int sumar(int a, int b) {
+            return a + b;
+        }
+
+        public double sumar(double a, double b) {
+            return a + b;
+        }
+    }
 }
