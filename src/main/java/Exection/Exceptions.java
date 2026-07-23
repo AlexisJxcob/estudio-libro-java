b65e736e5bc3d3a20cb8ff7cf8dc216803a096cf
Alexis Jacob <ferjacobka@gmail.com>
2026-07-23 10:32:03 -0400

    Chore: exceptions principios

---
+++ src/main/java/Exection/Exceptions.java
@@ -0,0 +1,17 @@
+package Exection;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+
+        // manejo de excepciones 
+        
+        try { // aca dentro va el codigo que posiblemente falle
+            
+        } catch () {
+        }
+        
+        var result = 10 / 0;
+        System.out.println(result);
+    }
+}
--- src/main/java/com/estudios/poo/Composition.java
+++ src/main/java/com/estudios/poo/Composition.java
@@ -1,18 +1,24 @@
 package com.estudios.poo;
 
 public class Composition {
+
     public static void main(String[] args) {
-        var auto = new Car();
-        auto.on();
-    }
-    
+        var car = new Car();
+        car.engine.on();    }
+
     public static class Engine {
-        public void on(){
+
+        public void on() {
             System.out.println("motor encendido");
         }
     }
-    
-    public static class Car extends Engine {
-        
+
+    public static class Car {
+
+        Engine engine = new Engine();
+
+        public void on() {
+            engine.on();
+        }
     }
 }
