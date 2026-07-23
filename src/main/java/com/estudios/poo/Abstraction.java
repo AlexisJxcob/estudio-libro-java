89d0c208eac29ff46a7a0c4f6cb5967eb9ef6022
Alexis Jacob <ferjacobka@gmail.com>
2026-07-22 20:55:09 -0400

    Chore: interface y abstraction exercises por hacer

--- src/main/java/com/estudios/poo/Abstraction.java
+++ src/main/java/com/estudios/poo/Abstraction.java
@@ -3,17 +3,27 @@ package com.estudios.poo;
 public class Abstraction {
 
     public static void main(String[] args) {
-        
+
         // var animalito = new Animal(); una clase abstracta no puede ser instanciada
         var doggy = new Dog();
         doggy.Sleep(); // si es una herencia si se puede instanciar
         doggy.Sound();
-        
+
         var kitty = new Cat();
         kitty.Sleep();
         kitty.Sound();
-        
+
         // - interface
+        
+        var batman = new Bat();
+        batman.Sleep();
+        batman.Sound();
+        batman.fly();
+        
+        var ave = new Bird();
+        ave.Sleep();
+        ave.Sound();
+        ave.fly();
     }
 
     // clase abstracta
@@ -22,9 +32,10 @@ public class Abstraction {
         public void Sleep() {
             System.out.println("EL animal duerme");
         }
-        
+
         public abstract void Sound();
     }
+
     public static class Dog extends Animal {
 
         @Override
@@ -36,42 +47,51 @@ public class Abstraction {
         public void Sound() {
             System.out.println("El perro hace Guau");
         }
-        
-        
+
     }
+
     public static class Cat extends Animal {
 
         @Override
         public void Sound() {
             System.out.println("EL gato hace miau");
         }
-        
+
     }
-    
-    
+
+    // - interface
+    public interface Fliying {
+
+        // no habra implementaciones
+        void fly();
+    }
+
     // estas 2 clases tienen 2 cosas similares
-    public static class Bird extends Animal {
+    public static class Bird extends Animal implements Fliying{
 
         @Override
         public void Sound() {
             System.out.println("EL ave hace pio pio");
         }
 
+        @Override
+        public void fly() {
+            System.out.println("Vuela vuela");
+        }
+
     }
-    
-    public static class Bat extends Animal {
+
+    public static class Bat extends Animal implements Fliying{ // palabra reservada por interface
 
         @Override
         public void Sound() {
             System.out.println("EL ave hace iam batman");
         }
 
-    }
-    
-    // - interface
-    
-    public interface Fliying {
-        // no habra implementaciones
-        void fly();
+        @Override
+        public void fly() {
+            System.out.println("Volando bajo, Robin");
+        }
+
     }
 }
